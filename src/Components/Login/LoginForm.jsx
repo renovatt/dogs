ae928e949528c5fbe96b702edbd70009ef6fced9
Will <wlymes@gmail.com>
2022-11-09 10:00:45 -0300

    GET and POST method added

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,34 +3,40 @@ import { Link } from 'react-router-dom'
 import { Input } from '../Forms/Input'
 import { Button } from '../Forms/Button'
 import { useForm } from '../../Hooks/useForm'
+import { TOKEN_POST, TOKEN_GET } from '../../api'
 
 export const LoginForm = () => {
 
   const username = useForm()
   const password = useForm()
 
+  React.useEffect(()=> {
+    const token = localStorage.getItem('token')
+    if(token){
+      getUser(token)
+    }
+  }, [])
+
+  async function getUser(token){
+    const { url, options } = TOKEN_GET(token)
+    const res = await fetch(url, options)
+    const json = await res.json()
+    console.log(json)
+  }
   
-  function handleSubmit(e){
+  async function handleSubmit(e){
     e.preventDefault()
-
     if(username.validate() && password.validate()){
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token',{
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify()
-    })
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value
+      })
 
-    .then(res => {
-      console.log(res)
-      return res.json()
-    })
-
-    .then(json => {
-      console.log(json)
-      return json
-    })
+    const res = await fetch(url, options)
+    const json = await res.json()
+    localStorage.setItem('token', json.token)
+    const token = localStorage.getItem('token')
+    getUser(token)
   }    
 }
 
@@ -38,13 +44,9 @@ export const LoginForm = () => {
     <div>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-       
       <Input label="Usuário" type="text" name={username} {...username}/>
-
       <Input label="Senha" type="password" name={password} {...password}/>
-
       <Button>Entrar</Button>
-
       </form>
       <Link to='/login/criar'>Cadastro</Link>
     </div>
